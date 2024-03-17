608cf6a22183b01e9093207ff3719f19d74dca41
Craig Lindley <craig@minaq.com>
2024-03-17 22:42:20 +0000

    icons sized correctly

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 	const [asideVisible, setAsideVisible] = useState(true);
 
 	return (
-		<div className="flex h-screen overflow-hidden">
+		<div className="flex h-screen overflow-hidden text-sm">
 			<div className={`bg-customgray h-full text-cusoffwhite border-r border-cusbuttonoutline transition-all duration-250 ease-in-out ${navVisible ? 'w-1/5' : 'w-0'}`}>
 				{navVisible && (
 					<nav>
@@ -15,12 +15,12 @@ function App() {
 				)}
 			</div>
 			<main className="bg-customgray h-full text-cusoffwhite flex-grow">
-				<header className="bg-customgray text-cusoffwhite p-2 flex justify-between items-center border-b border-cusbuttonoutline">
-					<button onClick={() => setNavVisible(!navVisible)} className="p-2 hover:bg-cusdarkmenubg">
-						{navVisible ? <BsLayoutSidebarInset className="w-5 h-5"/> : <BsLayoutSidebar className="w-5 h-5"/>}
+				<header className="bg-customgray text-cusoffwhite flex justify-between items-center border-b border-cusbuttonoutline">
+					<button onClick={() => setNavVisible(!navVisible)} className="p-2 hover:bg-cusdarkmenubg rounded-md">
+						{navVisible ? <BsLayoutSidebarInset className="w-4 h-4"/> : <BsLayoutSidebar className="w-4 h-4"/>}
 					</button>
-					<button onClick={() => setAsideVisible(!asideVisible)} className="p-2 hover:bg-cusdarkmenubg">
-						{asideVisible ? <BsLayoutSidebarInsetReverse className="w-5 h-5"/> : <BsLayoutSidebarReverse className="w-5 h-5"/>}
+					<button onClick={() => setAsideVisible(!asideVisible)} className="p-2 hover:bg-cusdarkmenubg rounded-md">
+						{asideVisible ? <BsLayoutSidebarInsetReverse className="w-4 h-4"/> : <BsLayoutSidebarReverse className="w-4 h-4"/>}
 					</button>
 				</header>
 				Main Content
